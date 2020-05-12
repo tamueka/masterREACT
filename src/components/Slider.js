79ca010d41bce4a0ec9f940c8e032fdcb2389828
tamueka <tamueka@hotmail.com>
2020-05-12 16:29:22 +0200

    creamos componente Slider + html en el render

---
+++ src/components/Slider.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Slider extends Component {
+  render() {
+    return (
+      <div id="slider" className="slider-big">
+        <h1>
+          APP React
+        </h1>
+        <a href="#" className="btn-white">
+          Ir al blog
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Slider;
