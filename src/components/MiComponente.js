80a6873b5df66080f2fe28bbad79b2d1b1839fc1
tamueka <tamueka@hotmail.com>
2020-05-12 14:07:32 +0200

    mas acerca los componentes + aplicamos logica al componente

--- src/components/MiComponente.js
+++ src/components/MiComponente.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 
-class MiComponente extends React.Component {
+class MiComponente extends Component {
   render() {
-    return <h1>Hola soy el componente llamado: MiComponente</h1>;
+    let receta = {
+      nombre: "Pizza",
+      ingredientes: ["Tomate", "Queso", "Jamon Cocido"],
+      calorias: 400,
+    };
+
+    return (
+      <div className="mi-componente">
+        <h1>{`Receta: ${receta.nombre}`}</h1>
+        <h2>{`Calorias: ${receta.calorias}`}</h2>
+        <ol>
+          {receta.ingredientes.map((ingrediente, i) => {
+            //console.log(ingrediente, i);
+            return <li key={i}>{ingrediente}</li>;
+          })}
+        </ol>
+        <hr></hr>
+      </div>
+    );
   }
 }
 
