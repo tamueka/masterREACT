5f6ff6ebc8b8f25131d584d07ebac5ac3f5f5f5a
tamueka <tamueka@hotmail.com>
2020-05-12 13:49:24 +0200

    creamos componente en react + organizamos por carpetas dentro src

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/images/logo.svg";
+import "./assets/css/App.css";
+
+//Importar Componentes
+import MiComponente from "./components/MiComponente";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className="componentes">
+          <MiComponente />
+        </section>
       </header>
     </div>
   );
