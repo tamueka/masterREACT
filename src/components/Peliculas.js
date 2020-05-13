01e6bfdd7d8de0af9a1d60d2fa3cd50518bd488c
tamueka <tamueka@hotmail.com>
2020-05-13 23:54:59 +0200

    metodos de ciclo de vida react componentWillMount() componentDidMount() componentWillUnmount()

--- src/components/Peliculas.js
+++ src/components/Peliculas.js
@@ -2,47 +2,59 @@ import React, { Component } from "react";
 import Pelicula from "./Pelicula";
 
 class Peliculas extends Component {
-  state = {
-    peliculas: [
-      {
-        titulo: "Batman vs Superman",
-        image:
-          "https://images-na.ssl-images-amazon.com/images/I/911RR0tzL2L._SY445_.jpg",
-      },
-      {
-        titulo: "Gran torino",
-        image:
-          "https://images-na.ssl-images-amazon.com/images/I/81TlD6oh2BL._SY445_.jpg",
-      },
-      {
-        titulo: "El señor de los anillos",
-        image:
-          "https://images-na.ssl-images-amazon.com/images/I/8112Bc4LpuL._SX342_.jpg",
-      },
-    ],
-    nombre: "Samuel Ruiz",
-    favorita: ''
-  };
+  state = {};
 
   cambiarTitulo = () => {
-
-    var {peliculas} = this.state
+    var { peliculas } = this.state;
     //var random = Math.floor(Math.random() * 3)
-    peliculas[0].titulo = "Batman Begins"
+    peliculas[0].titulo = "Batman Begins";
 
     this.setState({
-      peliculas: peliculas
-    })
-  }
+      peliculas: peliculas,
+    });
+  };
 
   favorita = (pelicula, indice) => {
-    console.log("FAVORITA MARCADA")
-    console.log(pelicula, indice)
+    console.log("FAVORITA MARCADA");
+    console.log(pelicula, indice);
     //console.log(event)
 
     this.setState({
-      favorita: pelicula
-    })
+      favorita: pelicula,
+    });
+  };
+
+  componentWillMount() {
+    console.log("Se va a montar el componente")
+    this.setState({
+      peliculas: [
+        {
+          titulo: "Batman vs Superman",
+          image:
+            "https://images-na.ssl-images-amazon.com/images/I/911RR0tzL2L._SY445_.jpg",
+        },
+        {
+          titulo: "Gran torino",
+          image:
+            "https://images-na.ssl-images-amazon.com/images/I/81TlD6oh2BL._SY445_.jpg",
+        },
+        {
+          titulo: "El señor de los anillos",
+          image:
+            "https://images-na.ssl-images-amazon.com/images/I/8112Bc4LpuL._SX342_.jpg",
+        },
+      ],
+      nombre: "Samuel Ruiz",
+      favorita: "",
+    });
+  }
+
+  componentDidMount(){
+    console.log("Componente montado")
+  }
+
+  componentWillUnmount(){
+    console.log("Componente desmontado")
   }
 
   render() {
@@ -50,14 +62,15 @@ class Peliculas extends Component {
       <div id="content" className="peliculas">
         <h2 className="subheader">Peliculas</h2>
         <h4>Lista de peliculas favoritas de {this.state.nombre}</h4>
-        <button onClick={this.cambiarTitulo}>Cambiar titulo pelicula Batman</button>
-        {
-          this.state.favorita && 
-        <p className="favorita">
-          <span>La pelicula favorita es: </span>
-          <strong>{this.state.favorita.titulo}</strong>
-        </p>
-        }
+        <button onClick={this.cambiarTitulo}>
+          Cambiar titulo pelicula Batman
+        </button>
+        {this.state.favorita && (
+          <p className="favorita">
+            <span>La pelicula favorita es: </span>
+            <strong>{this.state.favorita.titulo}</strong>
+          </p>
+        )}
 
         {/* COMPONENTE PELICULA */}
         {this.state.peliculas.map((pelicula, i) => {
