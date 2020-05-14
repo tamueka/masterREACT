fb117661edd42e4d5ceb35dc63447d4cc3b78ff9
tamueka <tamueka@hotmail.com>
2020-05-14 13:27:39 +0200

    Ruta para pagina 404

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import SeccionPruebas from './components/SeccionPruebas';
 import MiComponente from "./components/MiComponente";
 import Peliculas from "./components/Peliculas";
+import Error from "./components/Error";
+
 
 class Router extends Component {
   render() {
@@ -13,6 +15,7 @@ class Router extends Component {
           <Route exact path="/" component={Peliculas} />
           <Route exact path="/ruta-prueba" component={SeccionPruebas} />
           <Route exact path="/segunda-ruta" component={MiComponente} />
+          <Route component={Error} />
         </Switch>
       </BrowserRouter>
     );
---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <section id="center">
+    <h2 className="subheader">Página no encontrada</h2>
+    <p>La pagina a la que intentas acceder no existe en la web</p>
+    </section>
+  );
+};
+
+export default Error;
