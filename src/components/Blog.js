d49083507684b311db427a2bb7067cd093b2a10d
tamueka <tamueka@hotmail.com>
2020-05-16 19:33:24 +0200

    introduccion a http y peticiones asincronas con React utilizando la libreria axio

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,18 +1,44 @@
 import React, { Component } from "react";
+import axios from "axios";
 import Slider from "../components/Slider";
 import Sidebar from "../components/Sidebar";
 
 class Blog extends Component {
+  state = {
+    articles: {},
+    status: null,
+  };
+
   render() {
+    axios
+      .get("http://localhost:3900/api/articles")
+      .then((res) => {
+        //console.log(res.data)
+        this.setState({
+          articles: res.data.articles,
+          status: 'success'
+        });
+      })
+      .catch((err) => console.log(err))
+
     return (
-      <div id="home">
-        <Slider title="Blog" size="slider-small"/>
-        <div id="content">
-          <div className="center">
+      <div id="blog">
+        <Slider title="Blog" size="slider-small" />
+        <div className="center">
+          <div id="content">
             {/* LISTADO DE ARTICULOS DEL API */}
+            {this.state.status === "success" && (
+              <div>
+                {this.state.articles.map((article) => {
+                  return (
+                    <h1 key={article._id}>{article.title}</h1>
+                    )
+                })}
+              </div>
+            )}
           </div>
         </div>
-        <Sidebar blog="true"/>
+        <Sidebar blog="true" />
       </div>
     );
   }
