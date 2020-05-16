b5cd777ac46396415e23b55fd7dc41f16fadc872
tamueka <tamueka@hotmail.com>
2020-05-16 17:37:00 +0200

    Fromularios react 2 + evento onChange

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -10,19 +10,55 @@ class Formulario extends Component {
   generoMujerRef = React.createRef();
   generoOtroRef = React.createRef();
 
+  state = {
+    user: {},
+  };
+
   recibirFormulario = (e) => {
     e.preventDefault();
-    alert("Formulario enviado");
-    console.log(this.nombreRef.current.value);
-  }
+
+    let genero = "hombre";
+
+    if (this.generoHombreRef.current.checked) {
+      genero = this.generoHombreRef.current.value;
+    } else if (this.generoMujerRef.current.checked) {
+      genero = this.generoMujerRef.current.value;
+    } else {
+      genero = this.generoOtroRef.current.value;
+    }
+
+    const user = {
+      nombre: this.nombreRef.current.value,
+      apellidos: this.apellidosRef.current.value,
+      bio: this.bioRef.current.value,
+      genero: genero,
+    };
+
+    this.setState({
+      user: user,
+    });
+
+    console.log("Formulario enviado");
+    console.log(user);
+  };
 
   render() {
     return (
       <div id="formulario">
         <h1 className="subheader">Formulario</h1>
         <div className="center">
+          {/* MOSTRAMOS DATOS DEL FORMULARIO */}
+          {this.state.user.nombre && (
+            <div id="user-data">
+              <p>Nombre: {this.state.user.nombre}</p>
+              <p>Apellidos: {this.state.user.apellidos}</p>
+              <p>Biografia: {this.state.user.bio}</p>
+              <p>Genero: {this.state.user.genero}</p>
+            </div>
+          )}
           <section id="content">
-            <form className="mid-form" onSubmit={this.recibirFormulario}>
+            {/* CREAMOS FORMULARIO */}
+            <form className="mid-form" onSubmit={this.recibirFormulario} onChange={this.recibirFormulario}>
               <div className="form-group">
                 <label htmlFor="nombre">Nombre</label>
                 <input type="text" name="nombre" ref={this.nombreRef} />
@@ -30,18 +66,36 @@ class Formulario extends Component {
 
               <div className="form-group">
                 <label htmlFor="apellidos">Apellidos</label>
-                <input type="text" name="apellidos" ref={this.apellidosRef}  />
+                <input type="text" name="apellidos" ref={this.apellidosRef} />
               </div>
 
               <div className="form-group">
                 <label htmlFor="bio">Biografia</label>
-                <textarea name="bio" ref={this.bioRef} ></textarea>
+                <textarea name="bio" ref={this.bioRef}></textarea>
               </div>
 
               <div className="form-group radibuttons">
-                <input type="radio" name="genero" value="hombre" ref={this.generoHombreRef}/> Hombre
-                <input type="radio" name="genero" value="mujer" ref={this.generoMujerRef}/> Mujer
-                <input type="radio" name="genero" value="otro" ref={this.generoOtroRef}/> Otro
+                <input
+                  type="radio"
+                  name="genero"
+                  value="hombre"
+                  ref={this.generoHombreRef}
+                />{" "}
+                Hombre
+                <input
+                  type="radio"
+                  name="genero"
+                  value="mujer"
+                  ref={this.generoMujerRef}
+                />{" "}
+                Mujer
+                <input
+                  type="radio"
+                  name="genero"
+                  value="otro"
+                  ref={this.generoOtroRef}
+                />{" "}
+                Otro
               </div>
 
               <div className="clearfix"></div>
