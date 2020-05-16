e9b771bb70ae85f97f08a4893fe9102758a15998
tamueka <tamueka@hotmail.com>
2020-05-16 16:57:38 +0200

    formuarios en React 1

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,39 +1,52 @@
 import React, { Component } from "react";
-import Slider from "./Slider";
 import Sidebar from "./Sidebar";
 
 class Formulario extends Component {
+
+  nombreRef = React.createRef();
+  apellidosRef = React.createRef();
+  bioRef = React.createRef();
+  generoHombreRef = React.createRef();
+  generoMujerRef = React.createRef();
+  generoOtroRef = React.createRef();
+
+  recibirFormulario = (e) => {
+    e.preventDefault();
+    alert("Formulario enviado");
+    console.log(this.nombreRef.current.value);
+  }
+
   render() {
     return (
-      <div id="formualrio">
-        <Slider title="Formulario" size="slider-small" />
+      <div id="formulario">
+        <h1 className="subheader">Formulario</h1>
         <div className="center">
           <section id="content">
-            <form className="mid-form">
+            <form className="mid-form" onSubmit={this.recibirFormulario}>
               <div className="form-group">
-                <label for="nombre">Nombre</label>
-                <input type="text" name="nombre" />
+                <label htmlFor="nombre">Nombre</label>
+                <input type="text" name="nombre" ref={this.nombreRef} />
               </div>
 
               <div className="form-group">
-                <label for="apellidos">Apellidos</label>
-                <input type="text" name="apellidos" />
+                <label htmlFor="apellidos">Apellidos</label>
+                <input type="text" name="apellidos" ref={this.apellidosRef}  />
               </div>
 
               <div className="form-group">
-                <label for="bio">Biografia</label>
-                <textarea name="bio"></textarea>
+                <label htmlFor="bio">Biografia</label>
+                <textarea name="bio" ref={this.bioRef} ></textarea>
               </div>
 
               <div className="form-group radibuttons">
-                <input type="radio" name="genero" value="hombre" /> Hombre
-                <input type="radio" name="genero" value="mujer" /> Mujer
-                <input type="radio" name="genero" value="otro" /> Otro
+                <input type="radio" name="genero" value="hombre" ref={this.generoHombreRef}/> Hombre
+                <input type="radio" name="genero" value="mujer" ref={this.generoMujerRef}/> Mujer
+                <input type="radio" name="genero" value="otro" ref={this.generoOtroRef}/> Otro
               </div>
 
               <div className="clearfix"></div>
 
-              <input type="submit" value="Enviar" className="btn btn-success" />
+              <input type="submit" value="Enviar" className="btn" />
             </form>
           </section>
         </div>
