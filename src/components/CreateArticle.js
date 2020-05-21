b3e8cfed5eec555896d91cf7689d88e70abe5418
tamueka <tamueka@hotmail.com>
2020-05-21 18:20:58 +0200

    recoger datos del formulario de creacion

--- src/components/CreateArticle.js
+++ src/components/CreateArticle.js
@@ -6,21 +6,57 @@ import Global from "../Global";
 //Validacion formulario
 //Alertas
 
-class createArticle extends Component {
+class CreateArticle extends Component {
+  titleRef = React.createRef();
+  contentRef = React.createRef();
+
+  state = {
+    article: {},
+    status: null,
+  };
+
+  changeState = () => {
+    this.setState({
+      article: {
+        title: this.titleRef.current.value,
+        content: this.contentRef.current.value,
+      },
+    });
+   console.log(this.state)
+/*   console.log(this.state.article.title)
+    console.log(this.state.article.content); */
+  };
+
+  saveArticle = (e) => {
+    e.preventDefault();
+
+    //Rellenar state con formulario
+    this.changeState();
+  };
+
   render() {
     return (
       <div className="center">
         <section id="content">
           <h1 className="subheader">Crear articulo</h1>
-          <form className="mid-form">
+          <form className="mid-form" onSubmit={this.saveArticle}>
             <div className="form-group">
               <label htmlFor="title">Titulo</label>
-              <input type="text" name="title" />
+              <input
+                type="text"
+                name="title"
+                ref={this.titleRef}
+                onChange={this.changeState}
+              />
             </div>
 
             <div className="form-group">
               <label htmlFor="content">Contenido</label>
-              <textarea name="content" />
+              <textarea
+                name="content"
+                ref={this.contentRef}
+                onChange={this.changeState}
+              />
             </div>
 
             <div className="form-group">
@@ -36,4 +72,4 @@ class createArticle extends Component {
   }
 }
 
-export default createArticle;
+export default CreateArticle;
